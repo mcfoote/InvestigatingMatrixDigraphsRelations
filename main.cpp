811d9d1d9085c4e45e345eaf89791c4f314cb4d4
Mitchell C. Foote <mcfoote321@gmail.com>
2024-03-28 22:25:41 -0400

    base project files

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/*
+    Class: 314 Wed 1:00 pm Winter
+    Programmer: Mitchell Foote
+    Description: 
+*/
+
+#include<iostream>
+#include<string>
+#include<limits>
+#include<algorithm>
+#include<vector>
+#include<sstream>
+
+// Scoping
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::getline;
+using std::vector;
+
+// Function prototypes
+void investigator();
+
+
+int main() {
+
+    cout << "Matrix Digraph and Relations Investigator" << endl;
+
+    return 0;
+}
+
+void investigator() {
+    
+}
